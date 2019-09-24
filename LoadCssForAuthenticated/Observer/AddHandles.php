0064f41f8f827ccf2738ff3410804291823a0eaa
ahmed.goda <ahmed.goda@bytology.co.uk>
2019-09-24 18:09:35 +0200

    Review Code & Add new module
    - Review the code of LoadCssForAuthenticated.
    - Add qty buttons to PDP functionality.

--- LoadCssForAuthenticated/Observer/AddHandles.php
+++ LoadCssForAuthenticated/Observer/AddHandles.php
@@ -7,22 +7,20 @@ use Magento\Customer\Model\Session as CustomerSession;
 
 /**
  * Class AddHandles
- * @package Fc\CustomHandles\Observer
- * @author Frank Clark <mrfrankclark1@gmail.com>
  */
 class AddHandles implements ObserverInterface
 {
     /** @var CustomerSession  */
     protected $customerSession;
+
     /**
      * AddHandles constructor.
      * @param CustomerSession $customerSession
      */
-    public function __construct(
-        CustomerSession $customerSession
-    ) {
+    public function __construct(CustomerSession $customerSession) {
         $this->customerSession = $customerSession;
     }
+
     /**
      * Determine if the customer is logged in and if so add a layout handle
      * @param Observer $observer
@@ -32,8 +30,6 @@ class AddHandles implements ObserverInterface
         $layout = $observer->getEvent()->getLayout();
         if ($this->customerSession->isLoggedIn()) {
             $layout->getUpdate()->addHandle('customer_logged_in');
-        } else {
-            $layout->getUpdate()->addHandle('customer_logged_out');
         }
     }
 }
